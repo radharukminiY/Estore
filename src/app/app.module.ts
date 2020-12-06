90211c169fc206715213954fd6ecf1d3e21c45eb
radharukminiY <rukku558@gmail.com>
2020-12-07 01:52:23 +0530

    Basic view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { OrdersModule  } from './orders/orders.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+  RouterModule,
+  OrdersModule,
+  AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
